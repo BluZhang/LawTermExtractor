923befbc3f80065d476ad73126cba596540e7fe7
pku_liuqiang <pku_liuqiang@163.com>
2017-12-26 12:05:25 +0800

    将抽取出来的术语变成文件形式存储，每个文件存储对应的法条文本内容。

--- src/main/java/com/pku/law/term/filter/filter.kt
+++ src/main/java/com/pku/law/term/filter/filter.kt
@@ -13,9 +13,19 @@ import java.io.FileWriter
 fun main(args: Array<String>) {
     val head = createTransferMap()
     prepareDict()
+    val canDir = File("D:/term/candidate")
+    canDir.deleteRecursively()
+    canDir.mkdir()
     val map = mutableMapOf<String, Pair<String, Int>>()
+    val lastMap = mutableMapOf<String, String>()
+    val modelMap = mutableMapOf<String, MutableSet<String>>()
     File("D:/term/dict").listFiles().forEach {
         println(it.canonicalPath)
+        for(item in head.childrenMap.entries) {
+            if(item.value.endNum != 0) {
+                println(item.value)
+            }
+        }
         val jsonObj1 = JSONObject.fromObject(it.readText())
         val jsonArr1 = jsonObj1.getJSONArray("arr")
         for(i in 0..(jsonArr1.size - 1)) {
@@ -29,16 +39,28 @@ fun main(args: Array<String>) {
             assert(strList.size == posList.size)
             for(j in 0..(posList.size - 1)) {
                 var node = head
+                val sb = StringBuilder()
                 for(k in j..(posList.size - 1)) {
                     if(setAll.contains(posList[k]) || !node.childrenMap.keys.contains(posList[k])) {
                         break
                     }
-                    node = node.childrenMap.get(posList[k])!!;
+                    node = node.childrenMap.get(posList[k])!!
+                    sb.append("_${posList[k]}")
                     //判断在当前位置是否已经到了状态转移图中可以作为结尾的词性序列位置
                     if(node.endNum != 0) {
                         val key = strList.subList(j, k +1).joinToString("","","").replace("\"","")
-                        val pair = map.getOrDefault(key, Pair(strList.subList(j, k + 1).joinToString("--","","") + " -> " + posList.subList(j, k + 1).joinToString("_","","").replace("\"",""), 0))
+                        val pair = map.getOrDefault(key, Pair(strList.subList(j, k + 1).joinToString("--","","") + "->" + posList.subList(j, k + 1).joinToString("_","","").replace("\"",""), 0))
+                        //如果不全都是汉字，则直接跳过
+                        if(!key.matches("[\u4e00-\u9fa5]+".toRegex())) {
+                            break
+                        }
+                        if(!jsonArr2.toString().equals(lastMap.get(key))) {
+                            File("D:/term/candidate/$key.txt").appendText("$jsonArr2\r\n${jsonArr2.filterIndexed { index, any -> index % 2 == 0 }.joinToString("", "", "")}\r\n\r\n")
+                            lastMap.put(key, jsonArr2.toString())
+                        }
                         map.put(key, Pair(pair.first, pair.second + 1))
+                        modelMap.putIfAbsent(sb.toString().substring(1), mutableSetOf())
+                        modelMap.get(sb.toString().substring(1))!!.add(key)
                     }
                 }
             }
@@ -52,7 +74,12 @@ fun main(args: Array<String>) {
     outFile.createNewFile()
     val writer = BufferedWriter(FileWriter(outFile))
     map.toList().sortedByDescending { it.second.second }.forEach {
-        writer.write("${it.first}  ${it.second.first}  ${it.second.second}\n")
+        writer.write("${it.first} ${it.second.first} ${it.second.second}\n")
     }
     writer.close()
+    modelMap.forEach { k1, v1 ->
+        val writer1 = BufferedWriter(FileWriter(File("D:/term/cand/$k1")))
+        v1.sortedByDescending { it2 -> map.get(it2)!!.second }.forEach { it1 -> writer1.write("$it1 ${map.get(it1)!!.second}\r\n") }
+        writer1.close()
+    }
 }
