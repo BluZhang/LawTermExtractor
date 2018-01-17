038b15567e0f7f437efbf67e12ca346f50a0c83b
pku_liuqiang <pku_liuqiang@163.com>
2018-01-17 12:57:25 +0800

    行政法提取部分，写入了typeid为44和45的法律及法条。

--- src/main/java/com/pku/law/term/xzf/xzjdbc.kt
+++ src/main/java/com/pku/law/term/xzf/xzjdbc.kt
@@ -1,14 +1,13 @@
 package com.pku.law.term.xzf
 
-import com.pku.law.term.preprocess.Node
-import net.sf.json.JSONArray
-import net.sf.json.JSONObject
 import java.io.BufferedWriter
 import java.io.File
 import java.io.FileWriter
+import java.nio.charset.Charset
 import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.ResultSet
+import java.util.*
 
 //达梦数据库基本参数及配置
 const val driver = "dm.jdbc.driver.DmDriver"
@@ -16,34 +15,36 @@ const val url = "jdbc:dm://192.168.200.64:5678"
 const val username="SYSDBA"
 const val password="123123123"
 //从加入用户词典之后的分词结果中查询信息
-const val lawNameTypeSql="select ID, TITLE, LAWTYPE from FLFGK.ZLLAW_XZ where LAWTYPE=44 || LAWTYPE=45 order by LAWTYPE"
-//从未加入用户词典的分词结果中查询信息
-const val noDictSql="select ID, SEGMENTATION from TF_IDF.SEG_LQ_BEFORE order by ID"
+const val lawNameTypeSql="select ID, TITLE, TYPEID from FLFGK.ZLLAW_XZ where TYPEID=44 or TYPEID=45 order by TYPEID"
+
+const val lawItemSql="select ID, LAWID, CONTENT from FLFGK.ZLLAWCOLUMN_XZ where TYPEID=44 or TYPEID=45 order by TYPEID"
 //加入词典后的分词结果存入的目录
-const val dictDir = "dict"
-//未加入词典的分词结果存入的目录
-const val noDictDir = "noDict"
+const val dictDir = "lawNameId"
 //数据库连接
 var conn: Connection? = null
-
-//加入用户词典后，对分词产生实际效果的用户词及出现次数
-val mapOccur: MutableMap<String, Int> = mutableMapOf()
-//所有用户词典中用户词的集合
-val setAll: MutableSet<String> = mutableSetOf()
+val lawIdMap = mutableMapOf<Int, String>()
 
 fun main(args: Array<String>) {
     createConnection()
-    itemWrite()
+//    itemWrite()
+    lawNameTypeInit()
+    lawItemWrite()
+//    lawNameChange()
+}
+
+fun lawNameTypeInit() {
+//    File("D:/xzf/44.txt").forEachLine { if(it.isNotBlank()) { val splits = it.split("\t"); lawIdMap.put(splits[0].toInt(), splits[1]) } }
+    File("D:/xzf/45.txt").forEachLine { if(it.isNotBlank()) { val splits = it.split("\t"); lawIdMap.put(splits[0].toInt(), splits[1]) } }
 }
 
 //将数据库查询结果写入到文件当中。type用于标识，加入用户词典后的结果和未加入用户词典的结果在不同的文件目录之下。
 fun lawNameTypeAddToFile(rs: ResultSet, type: String) {
     val forbit = listOf("意见", "命令", "决定", "目录", "批复", "复函", "通知",
-            "草案", "暂行规定", "国务院令第", "废止", "修改", "决议")
+            "草案", "暂行规定", "国务院令第", "废止", "修改", "决议", "补充规定")
     val permit = listOf("条例", "办法", "规定", "细则", "法", "法（")
     val dir = "D:/xzf/$type"
-    if(!File("$dir/44").exists()) File("$dir/44").mkdir()
-    if(!File("$dir/45").exists()) File("$dir/45").mkdir()
+    if(!File("$dir/44").exists()) File("$dir/44").mkdirs()
+    if(!File("$dir/45").exists()) File("$dir/45").mkdirs()
     val writer44 = BufferedWriter(FileWriter("D:/xzf/44.txt"))
     val writer45 = BufferedWriter(FileWriter("D:/xzf/45.txt"))
     val map = mutableMapOf<Int, BufferedWriter>()
@@ -73,6 +74,17 @@ fun createConnection(): Unit {
     conn = DriverManager.getConnection(url, username, password)
 }
 
+fun lawNameChange() {
+    val set44 = mutableSetOf<String>()
+    File("D:/xzf/44.txt").forEachLine { if(!it.contains("修正")) { set44.add(it) } }
+    File("D:/xzf/44.txt").delete()
+    File("D:/xzf/44.txt").writeText(set44.joinToString("\r\n","",""))
+    val set45 = mutableSetOf<String>()
+    File("D:/xzf/45.txt").forEachLine { if(!it.contains("修正")) { set45.add(it) } }
+    File("D:/xzf/45.txt").delete()
+    File("D:/xzf/45.txt").writeText(set45.joinToString("\r\n","",""))
+}
+
 //从数据库中读取法律法规id、名称及对应分类
 fun itemWrite() {
     //预编译dictSql语句
@@ -87,4 +99,33 @@ fun itemWrite() {
     pstmt1.close()
     //关闭数据库连接
     conn!!.close()
+}
+//从数据库中读取法律法规id、名称及对应分类
+fun lawItemWrite() {
+    //预编译dictSql语句
+    val pstmt1 = conn!!.prepareStatement(lawItemSql)
+    val rs1 = pstmt1.executeQuery()
+    if(!File("D:/xzf").exists()) File("D:/xzf").mkdir()
+//    if(!File("D:/xzf/44").exists()) File("D:/xzf/44").mkdir()
+    if(!File("D:/xzf/45").exists()) File("D:/xzf/45").mkdir()
+    //将加入用户词典之后的分词结果，写入文件当中
+    lawItemAddToFile(rs1, "45")
+    rs1.close()
+    pstmt1.close()
+    //关闭数据库连接
+    conn!!.close()
+}
+
+fun lawItemAddToFile(rs: ResultSet, type: String) {
+    val writerMap = mutableMapOf<String, BufferedWriter>()
+    while (rs.next()) {
+        val id = rs.getInt("ID")
+        val lawId = rs.getInt("LAWID")
+        val content = rs.getString("CONTENT")
+        if(lawIdMap.keys.contains(lawId)) {
+            if(!writerMap.keys.contains(lawId.toString())) { writerMap.put(lawId.toString(), BufferedWriter(FileWriter(File("D:/xzf/$type/${lawIdMap.get(lawId)}.txt")))) }
+            writerMap.get(lawId.toString())!!.write("$id^^${String(Base64.getEncoder().encode(content.toByteArray(Charset.forName("UTF-8"))), Charset.forName("UTF-8"))}\r\n")
+        }
+    }
+    writerMap.values.forEach { it.close() }
 }
